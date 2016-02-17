6b1699654f09f9a3a40d2c34b12dc11e102bd78e
Flávio Codeço Coelho <fccoelho@gmail.com>
2016-02-17 10:05:03 -0200

    Nikola auto commit.
    Nikola version: 7.7.5

--- conf.py
+++ conf.py
@@ -279,7 +279,7 @@ COMPILERS = {
 # Nikola supports logo display.  If you have one, you can put the URL here.
 # Final output is <img src="LOGO_URL" id="logo" alt="BLOG_TITLE">.
 # The URL may be relative to the site root.
-LOGO_URL = 'galleries/logo.png'
+LOGO_URL = 'galleries/logo_pq.png'
 
 # If you want to hide the title of your website (for example, if your logo
 # already contains the text), set this to False.
