25729569681a3b482f4719a1e8724a81c260e2f8
Flávio Codeço Coelho <fccoelho@gmail.com>
2016-02-17 09:59:41 -0200

    Nikola auto commit.
    Nikola version: 7.7.5

--- conf.py
+++ conf.py
@@ -140,9 +140,9 @@ NAVIGATION_LINKS = {
 }
 
 # Name of the theme to use.
-THEME = "octopress"
-# THEME = "material-theme"
-#THEME = "bootstrap3"
+# THEME = "octopress"
+THEME = "material-theme"
+# THEME = "bootstrap3"
 
 # Primary color of your theme. This will be used to customize your theme and
 # auto-generate related colors in POSTS_SECTION_COLORS. Must be a HEX value.
